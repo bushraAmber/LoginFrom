2df118fe030eef0f9acd1158e72db27aa35a5534
Superior <palakjani2283@gmail.com>
2022-02-16 13:55:44 +0500

    added signupfrom

--- app/src/main/java/com/example/labtask1/signupactivity.java
+++ app/src/main/java/com/example/labtask1/signupactivity.java
@@ -6,16 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class loginactivity extends AppCompatActivity {
+public class signupactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loginactivity);
-    }
-    public void  onloginbuttonClicked (View l){
-        Intent intent = new Intent(loginactivity.this,signupactivity.class);
-        startActivity(intent);
-    }
+        setContentView(R.layout.activity_signupactivity);
 
+    }
 }
